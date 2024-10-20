5e108c6e7385845a88e5fab4a785554e06754eb2
Serhii Safonov <137483700+KaratSergio@users.noreply.github.com>
2024-10-20 10:20:23 +0300

    Update SongAdd.tsx

--- entities/songs/ui/SongAdd.tsx
+++ entities/songs/ui/SongAdd.tsx
@@ -1,7 +1,35 @@
-import React from "react";
+import React, { useState } from "react";
+import StepWrapper from "@/shared/ui/StepWrapper";
+import { Button, Box } from "@mui/material";
 
 const SongAdd = () => {
-  return <div></div>;
+  const [activeStep, setActiveStep] = useState(0);
+
+  const handleNext = () => {
+    if (activeStep !== 2) {
+      setActiveStep((prev) => prev + 1);
+    }
+  };
+
+  const handleBack = () => {
+    setActiveStep((prev) => prev - 1);
+  };
+
+  return (
+    <div>
+      <StepWrapper activeStep={activeStep}>
+        {activeStep === 0 && <h1>STEP 1</h1>}
+        {activeStep === 1 && <h1>STEP 2</h1>}
+        {activeStep === 2 && <h1>STEP 3</h1>}
+      </StepWrapper>
+      <Box>
+        <Button disabled={activeStep === 0} onClick={handleBack}>
+          Back
+        </Button>
+        <Button onClick={handleNext}>Next</Button>
+      </Box>
+    </div>
+  );
 };
 
 export default SongAdd;
