0d182a6270b02209a633e3d84671df6d0334414f
Serhii Safonov <137483700+KaratSergio@users.noreply.github.com>
2024-10-30 13:45:05 +0200

    update Player

--- shared/ui/player/Player.tsx
+++ shared/ui/player/Player.tsx
@@ -1,6 +1,6 @@
 import { useEffect } from 'react';
 import { IconButton, CardContent, Typography } from '@mui/material';
-import { Pause, PlayArrow, VolumeUp } from '@mui/icons-material';
+import { Pause, PlayArrow, VolumeDownRounded, VolumeUp, VolumeOff } from '@mui/icons-material';
 import { useAppSelector } from '@/hooks/useAppSelector';
 import { usePlayerActions } from '@/hooks/usePlayerActions';
 import { selectAllPlayer } from '@/shared/redux/selectors/playerSelectors';
@@ -14,16 +14,27 @@ interface PlayerProps {
 let audio: HTMLAudioElement | null = null;
 
 const Player: React.FC<PlayerProps> = ({ song }) => {
-  const { isPaused } = useAppSelector(selectAllPlayer);
-  const { playSong, pauseSong } = usePlayerActions();
+  const { isPaused, volume, currentTime, duration } = useAppSelector(selectAllPlayer);
+  const { playSong, pauseSong, setVolumeSong, setCurrentTimeSong, setDurationSong } = usePlayerActions();
 
   useEffect(() => {
     if (!audio) {
       audio = new Audio(song.audio);
+      audio.volume = volume / 100;
+      audio.onloadedmetadata = () => {
+        if (audio?.duration !== undefined) setDurationSong(Math.ceil(audio.duration));
+      };
+      audio.ontimeupdate = () => {
+        if (audio?.currentTime !== undefined) setCurrentTimeSong(Math.ceil(audio.currentTime));
+      };
     }
-  }, [song.audio]);
+  }, [song.audio, setCurrentTimeSong, setDurationSong, volume]);
 
-  const play = () => {
+  useEffect(() => {
+    if (audio) audio.volume = volume / 100;
+  }, [volume]);
+
+  const togglePlay = () => {
     if (isPaused) {
       playSong();
       audio?.play();
@@ -33,27 +44,31 @@ const Player: React.FC<PlayerProps> = ({ song }) => {
     }
   };
 
+  const changeVolume = (e: React.ChangeEvent<HTMLInputElement>) => setVolumeSong(Number(e.target.value));
+  const changeCurrentTime = (e: React.ChangeEvent<HTMLInputElement>) => setCurrentTimeSong(Number(e.target.value));
+
   return (
     <div className="flex fixed bottom-0 items-center p-4 h-16 w-[900px] bg-gray-400">
-      <IconButton
-        onClick={(e) => {
-          e.stopPropagation();
-          play();
-        }}
-      >
-        {isPaused ? <PlayArrow /> : <Pause />}
-      </IconButton>
+      <IconButton onClick={togglePlay}>{isPaused ? <PlayArrow /> : <Pause />}</IconButton>
       <CardContent>
         <Typography component="div" variant="h5">
           {song.title}
         </Typography>
-        <Typography variant="subtitle1" color="text.secondary" component="div">
+        <Typography variant="subtitle1" color="text.secondary">
           {song.artist}
         </Typography>
       </CardContent>
-      <SongProgress left={0} right={100} onChange={() => {}} />
-      <VolumeUp className="ml-auto" />
-      <SongProgress left={0} right={100} onChange={() => {}} />
+      <SongProgress left={currentTime} right={duration} onChange={changeCurrentTime} />
+
+      {volume === 0 ? (
+        <VolumeOff className="ml-auto" />
+      ) : volume < 50 ? (
+        <VolumeDownRounded className="ml-auto" />
+      ) : (
+        <VolumeUp className="ml-auto" />
+      )}
+
+      <SongProgress left={volume} right={100} onChange={(e) => changeVolume(e)} />
     </div>
   );
 };
--- shared/ui/player/SongProgress.tsx
+++ shared/ui/player/SongProgress.tsx
@@ -3,13 +3,13 @@ import React from 'react';
 interface SongProgressProps {
   left: number;
   right: number;
-  onChange: () => void;
+  onChange: (e: React.ChangeEvent<HTMLInputElement>) => void;
 }
 
 const SongProgress: React.FC<SongProgressProps> = ({ left, right, onChange }) => {
   return (
     <div className="flex">
-      <input type="range" min={left} max={right} value={left} onChange={onChange} />
+      <input type="range" min={0} max={right} value={left} onChange={onChange} />
       <div>
         {left}/{right}
       </div>
